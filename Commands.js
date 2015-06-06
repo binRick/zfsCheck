ff02dfbdb969f88dee2d310d2336a294f0dbe314
binRick <rapidvps@gmail.com>
2015-06-06 18:01:27 -0400

    ideas

--- Commands.js
+++ Commands.js
@@ -1,21 +1,27 @@
 module.exports.zfsInfo = [{
-        key: 'availableBytes',
-        'title': 'Available Bytes',
-        cmd: 'zfs get -H -o value -p available tank',
-    }, {
-        key: 'poolHealth',
-        'title': 'Pool Health',
-        cmd: 'zpool get -H -p health tank',
-        process: function(stdOut) {
-            return stdOut.split('\t')[2];
-        },
-    }, {
-        key: 'filesystems',
-        'title': 'Filesystems',
-        cmd: 'zfs list -H -o name -p | grep ^tank/ | grep ^tank/Snapshots/ -v',
-        process: function(stdOut) {
-            return stdOut.split('\n');
-        },
-    }];
-module.exports.Install = [];
-        //cmd: 'cd /root && ls zfsCheck || git clone https://github.com/binRick/zfsCheck',
+    key: 'availableBytes',
+    'title': 'Available Bytes',
+    cmd: 'zfs get -H -o value -p available tank',
+}, {
+    key: 'poolHealth',
+    'title': 'Pool Health',
+    cmd: 'zpool get -H -p health tank',
+    process: function(stdOut) {
+        return stdOut.split('\t')[2];
+    },
+}, {
+    key: 'filesystems',
+    'title': 'Filesystems',
+    cmd: 'zfs list -H -o name -p | grep ^tank/ | grep ^tank/Snapshots/ -v',
+    process: function(stdOut) {
+        return stdOut.split('\n');
+    },
+}];
+module.exports.Install = [{
+    key: 'installZfsCheck',
+    'title': 'Install zfsCheck',
+    cmd: 'cd /root && ls zfsCheck || git clone https://github.com/binRick/zfsCheck',
+    process: function(stdOut) {
+        return stdOut.split('\n');
+    },
+}];
